2736e57f29f0d94bbf1df3c536e7ef43c361e392
jinqing.sjq <jinqing.sjq@alibaba-inc.com>
2017-07-03 19:18:27 +0800

    mybatis OK

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,24 +1,39 @@
 package controller;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 @Controller
 @RequestMapping(value = "/")
 public class MainController {
 
+    //测试函数，测试spring-MVC框架
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public String welcome() {
         return "hello world main controller";
     }
 
-    @RequestMapping("/index")
+    //测试函数，测试mybatis是否配置成功
+    @RequestMapping("/testBatis")
     @ResponseBody
-    public String index() {
-        return "hello world main index";
+    public String index() throws IOException {
+        String resource = "conf.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        String string;
+        string = session.selectOne("userMapper.findUserById", 101);//测试mybatis
+        return string;
     }
 }
